dd42917b3398d9b65b9a845be15b7086bd369b5b
KnjazS <knjazshugai@mail.ru>
2020-12-03 17:00:52 +0200

    Урок 8, задание 3

--- Урок 8/main.py
+++ Урок 8/main.py
@@ -91,8 +91,52 @@ def case_2():
         print(f"Error detected: {exc}")
 
 
+# 3. Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+# Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять список
+# только числами. Класс-исключение должен контролировать типы данных элементов списка.
+# Примечание: длина списка не фиксирована. Элементы запрашиваются бесконечно, пока пользователь сам не остановит работу
+# скрипта, введя, например, команду “stop”. При этом скрипт завершается, сформированный список с числами выводится на
+# экран.
+# Подсказка: для данного задания примем, что пользователь может вводить только числа и строки. При вводе пользователем
+# очередного элемента необходимо реализовать проверку типа элемента и вносить его в список, только если введено число.
+# Класс-исключение должен не позволить пользователю ввести текст (не число) и отобразить соответствующее сообщение.
+# При этом работа скрипта не должна завершаться.
+class NotNumbersOnlyException(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+def append(values: list[int], new_value: str):
+    try:
+        values.append(int(new_value))
+    except ValueError as exc:
+        raise NotNumbersOnlyException(exc.args)
+
+
+def input_numbers(prompt: str, error_message: str) -> list[int]:
+    result = []
+    while True:
+        values = input(prompt)
+        values = values.split()
+        for value in values:
+            if value == "stop":
+                return result
+            try:
+                append(result, value)
+            except NotNumbersOnlyException:
+                print(error_message.format(value))
+
+
+def case_3():
+    numbers = input_numbers("Введите числа (или stop для остановки ввода) --> ",
+                            "Пропущено значение '{0}' - это не целое число.")
+    print(f"Введены числа {numbers}")
+
+
 if __name__ == "__main__":
     # print("Задание 1:\n")
     # case_1()
-    print("\nЗадание 2:\n")
-    case_2()
+    # print("\nЗадание 2:\n")
+    # case_2()
+    print("\nЗадание 3:\n")
+    case_3()
